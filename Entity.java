1318847c0ed645f2a47f8dbc9248552484f18d1c
Ankush Pandey <80837710+ankush43@users.noreply.github.com>
2025-11-06 12:38:47 +0530

    Create Entity.java

---
+++ Entity.java
@@ -0,0 +1,18 @@
+package com.example.studentms.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String course;
+
+    // getters & setters
+}
